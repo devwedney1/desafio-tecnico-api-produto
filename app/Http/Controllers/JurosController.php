854ad210d92e884112f13610786da43717d4f364
Paulo Tavares <giovannapaulo80@gmail.com>
2025-06-11 11:01:06 -0300

    pequena correção no juroscontroller

--- app/Http/Controllers/JurosController.php
+++ app/Http/Controllers/JurosController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\DAO\JurosDAO;
-use App\DAO\ComprarDAO;
 use App\Model\taxaJuros;
 use DateTime;
 use Exception;
@@ -55,40 +54,32 @@ class JurosController
             }
 
             $valorTotal = 0.0;
-            $quantidade = 0;
 
             foreach ($valoresApi as $valor) {
                 if (isset($valor['valor'])) {
                     $valorTotal += floatval(str_replace(',', '.', $valor['valor']));
-                    $quantidade++;
                 }
             }
 
-            if ($quantidade === 0) {
-                $response->getBody()->write(json_encode(["erro" => "Nenhum valor válido retornado."]));
-                return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
-            }
+// Arredonda para 2 casas decimais
+            $valorTotal = round($valorTotal, 2);
 
-            $media = round($valorTotal / $quantidade, 4);
-
-            // Salva a nova taxa de juros no banco
+            // Salva a taxa acumulada no banco
             $jurosModel = new taxaJuros(
                 $dados['dataInicio'],
                 $dados['dataFinal'],
-                $media,
-                uniqid() // Gera um ID único para essa taxa
+                $valorTotal,
+                uniqid()
             );
             $jurosDao = new JurosDAO();
             $jurosDao->salvarJuros($jurosModel);
 
-            // (Opcional) Atualiza todas as compras com essa nova taxa, se necessário
-            // $comprasDao = new ComprarDAO();
-            // $comprasDao->atualizarBDCompras($media);
-
             $response->getBody()->write(json_encode([
                 "sucesso" => "A taxa de juros foi atualizada com sucesso.",
-                "novaTaxa" => $media
+                "taxaAcumulada" => $valorTotal
             ]));
+
+
             return $response->withStatus(200)->withHeader('Content-Type', 'application/json');
 
         } catch (Exception $e) {
